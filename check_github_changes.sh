8e2f2f8b9a083761e04cb4186e1babe40b61c312
Bytesmith Ahmad <b-9k@outlook.com>
2024-05-18 18:52:25 -0400

    ..

--- check_github_changes.sh
+++ check_github_changes.sh
@@ -9,39 +9,9 @@ home_color="\033[38;5;208m" # Orange for home
 journal_color="\033[96m"    # Cyan for journal
 reset_color="\033[0m"
 
-# Synchronize home
-echo -en "${home_color}Home: ${reset_color}"
-
-# Define your GitHub repository URL
-github_repo="$github/home"
-
-# Check for changes in the remote repository
-git remote update > /dev/null 2>&1
-
-# # Check if there are any changes
-# if git status -uno | grep -q 'Your branch is behind'; then
-    # # Changes found, prompt the user
-    # read -p "Found changes in the remote repository. Do you want to pull? (Y/N): " answer
-    # if [[ "$answer" =~ ^[Yy]$ ]]; then
-        # # User confirmed, pull changes
-        # git pull "$github_repo"
-                # 
-        # read -n1 -p "Press key to reset..."
-        # reset
-    # else
-        # echo "Changes not pulled."
-    # fi
-# else
-    # echo "No changes found in the remote repository."
-# fi
-
 # Synchronize rc-files
-echo -en "Configurations: "
+echo -en "Configuration: "
 git -C "$HOME/rc-files" pull
-
-# Synchronize bin
-echo -en "Scripts: "
-git -C "$HOME/bin" pull
 
 # Synchronize password-store
 echo -en "${password_color}Passwords: ${reset_color}"
@@ -62,3 +32,7 @@ git -C "$HOME/bills" pull
 # Synchronize tasks
 echo -en "${task_color}Task: ${reset_color}"
 git -C tasks pull
+
+# Synchronize bin
+echo -en "Bin: "
+git -C "$HOME/bin" pull
